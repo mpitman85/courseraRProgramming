bce4500c20539934a5c674c50c538fd5c4b1a28e
mpitman85 <megan.pitman@gmail.com>
2017-03-06 11:14:53 -0700

    updating work on course and photo practice

--- ProgAssignmentWeek3.R
+++ ProgAssignmentWeek3.R
@@ -37,3 +37,47 @@ test <- makeVector(data)
 cachemean(test)
 cachemean(test)
 cachemean(makeVector(data))
+
+##Working on matrix assignment
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function()x
+        setmatrix <- function(matrix) m <<- matrix
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        m <- x$getmatrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
+}
+
+##Testing
+test <- rbind(c(1, -1/4), c(-1/4,1))
+test
+class(test)
+mat <- makeCacheMatrix(test)
+mat
+inv <- cacheSolve(mat)
+test %*% inv
--- photodatapractice.R
+++ photodatapractice.R
@@ -1,11 +1,28 @@
 setwd("/Users/meganpitman/Dropbox/Projects/Classes/Coursera/DataScienceSpecialization/RProgramming/courseraRProgramming")
 
 data <- read.csv("KAFBexport151001to161001duplicaterows.csv")
-names(data)
-species <- split(data, data$Group_Species_LatinName)
-species
-sapply(species, function(x) colSums(x[,]))
-data$CameraPosition_Grid <- as.character(data$CameraPosition_Grid)
-data$CameraPosition_Grid
-levels(data$CameraPosition_Grid)
-data$CameraPosition_Grid <- as.factor(data$CameraPosition_Grid)
+
+install.packages("dplyr")
+library(dplyr)
+
+date <- data$DateTaken ##Set DateTaken column to date vector
+date <- as.character(date) ##Change date vector class to character
+date <- strptime(date, "%m/%d/%y %H:%M") ##Change date vector class to POSIXlt time format
+date <- as.POSIXct(date) ##Change date vector class to POSIXct time format
+class(date) ##Check that class is correct
+data$DateTaken <- date ##replace DateTaken column (formatted as levels) with date format
+class(data$DateTaken) ##Check that column still has POSIXct format
+data <- mutate(data, year=as.POSIXlt(data$DateTaken)$year +1900) ##Create year column
+data <- mutate(data, month=as.POSIXlt(data$DateTaken)$mon+1) ##Create month column
+data$count <- 1 ##Create count column
+data <- mutate(data, GroupTotal=Group_Male + Group_Female + Group_Juvenile + 
+                       Group_Indeterminate) ##Create GroupTotal column
+head(data) ##Look at data
+tail(data) ##Look at data
+with(data, tapply(GroupTotal, ##Create table of species totals by month
+            list(Group_Species_LatinName, month), sum)) 
+
+
+
+
+
