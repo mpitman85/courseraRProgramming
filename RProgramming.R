15a8b1bba2cd1f158c7829f3ce88d00eaaad2a18
mpitman85 <megan.pitman@gmail.com>
2017-03-29 10:31:39 -0400

    updated progress on course

--- RProgramming.R
+++ RProgramming.R
@@ -251,4 +251,64 @@ printmessage2 <- function(x) {
 }
 x <- log(-1)
 printmessage2(x)
-  
+ 
+##Week 4: Simulating a Linear Model
+set.seed(20)
+x <- rnorm(100)
+e <- rnorm(100, 0, 2)
+y <- 0.5 + 2 * x + e
+summary(y)
+plot(x,y)
+
+set.seed(10)
+x <- rbinom(100, 1, 0.5)
+e <- rnorm(100, 0, 2)
+y <- 0.5 + 2 * x + e
+summary(y)
+plot(x,y)
+
+set.seed(1)
+x <- rnorm(100)
+log.mu <- 0.5 + 0.3 * x
+y <- rpois(100, exp(log.mu))
+summary(y)
+plot(x, y)
+
+##Simulating Random Sampling
+set.seed(1)
+sample(1:10, 4)
+sample(1:10, 4)
+sample(letters, 5)
+sample(1:10)
+sample(1:10)
+sample(1:10, replace=TRUE)
+
+##R Profiler
+system.time(readLines("http://www.jhsph.edu"))
+hilbert <- function(n) {
+        i <- 1:n
+        1 / outer(i - 1, i, "+")
+}
+x <- hilbert(1000)
+system.time(svd(x))
+system.time(x)
+x
+
+system.time({
+        n <- 1000
+        r <- numeric(n)
+        for (i in 1:n) {
+                x <- rnorm(n)
+                r[i] <- mean(x)
+        }
+})
+
+##Quiz Week 4
+set.seed(1)
+rpois(5,2)
+
+set.seed(10)
+x <- rep(0:1, each=5)
+e <- rnorm(10,0,20)
+y <- 0.5 + 2*x +e
+plot(x,y)
